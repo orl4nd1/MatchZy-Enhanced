e709035844b87890ed79fe4d0fe0b50397cf688c
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2025-11-15 14:01:03 +0100

    chore: bump version to 0.8.20

--- MatchZy.cs
+++ MatchZy.cs
@@ -14,7 +14,7 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "0.8.19";
+        public override string ModuleVersion => "0.8.20";
 
         public override string ModuleAuthor => "WD- (https://github.com/shobhit-pathak/)";
 
