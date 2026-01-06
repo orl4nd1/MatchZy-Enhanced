e476d8b4591225c8f61fcb0c6069001ddc31ac73
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2026-01-06 16:42:08 +0100

    Release v1.2.2

--- src/MatchZy.cs
+++ src/MatchZy.cs
@@ -14,7 +14,7 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "1.2.1";
+        public override string ModuleVersion => "1.2.2";
 
         public override string ModuleAuthor => "WD- (https://github.com/shobhit-pathak/)";
 
