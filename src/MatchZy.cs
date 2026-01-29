4007d4901b6c70f676b1d47c42f5c4643b92de7b
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2026-01-29 10:23:52 +0100

    Release v1.4.13

--- src/MatchZy.cs
+++ src/MatchZy.cs
@@ -14,7 +14,7 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "1.4.12";
+        public override string ModuleVersion => "1.4.13";
 
         public override string ModuleAuthor => "sivert (https://github.com/sivert-io/)";
 
